2dbc889a744d0661928e5e46d8172dcbfcc5e8e5
t(h)inkerer <thinkerer@mennisch.net>
2022-02-08 16:29:02 -0500

    Switch example to fetchgit

--- configuration.example.nix
+++ configuration.example.nix
@@ -1,9 +1,10 @@
 # This is an example of how to use these nixpkgs and modules on a server.
 { ... } :
 let
-  branch = "main";
-  url = "https://github.com/mennisch/nixpkgs/archive/${branch}.tar.gz";
-  override = import (builtins.fetchTarball url);
+  override = import (builtins.fetchGit {
+    url = "git@github.com:mennisch/nixpkgs.git";
+    ref = "main";
+  });
   mennisch = override {};
 in {
   imports = [ (mennisch.modules.init override "<HOST>") ];
