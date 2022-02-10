c6661e10767a98eddd0b88447067b1362cbd660e
t(h)inkerer <thinkerer@mennisch.net>
2022-02-10 08:16:01 -0500

    Factor out defaults for restic backups

--- modules/restic.nix
+++ modules/restic.nix
@@ -1,5 +1,17 @@
 { config, lib, pkgs, ... }: let
   cfg = config.services;
+  defaults = {
+    environmentFile = "/var/lib/restic/environment";
+    passwordFile = "/var/lib/restic/repository-passphrase";
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 5"
+      "--keep-monthly 12"
+      "--keep-yearly 75"
+    ];
+    repository = "s3:s3.amazonaws.com/mennisch-restic";
+    timerConfig.OnCalendar = "hourly";
+  };
 in lib.mkMerge [
   {
     environment.systemPackages = [ pkgs.restic ];
@@ -7,28 +19,20 @@ in lib.mkMerge [
   }
   (lib.mkIf cfg.postgresql.enable
     {
-      services.restic.backups.postgresql = {
-        environmentFile = "/var/lib/restic/environment";
-        passwordFile = "/var/lib/restic/repository-passphrase";
+      services.restic.backups.postgresql = defaults // {
         paths = [ "/var/backup/postgresql" ];
-        repository = "s3:s3.amazonaws.com/mennisch-restic";
-        timerConfig.OnCalendar = "hourly";
         user = "postgres";
       };
       users.groups.restic.members = [ "postgres" ];
     })
   (lib.mkIf cfg.vaultwarden.enable
     {
-      services.restic.backups.vaultwarden = {
-        environmentFile = "/var/lib/restic/environment";
+      services.restic.backups.vaultwarden = defaults // {
         extraBackupArgs = [
           "--exclude=/var/lib/bitwarden_rs/icon_cache"
           "--exclude=/var/lib/bitwarden_rs/sends"
         ];
-        passwordFile = "/var/lib/restic/repository-passphrase";
         paths = [ "/var/lib/bitwarden_rs" ];
-        repository = "s3:s3.amazonaws.com/mennisch-restic";
-        timerConfig.OnCalendar = "hourly";
         user = "vaultwarden";
       };
       users.groups.restic.members = [ "vaultwarden" ];
