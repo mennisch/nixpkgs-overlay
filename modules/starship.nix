6706de22729d3b5a968322fa640d2a76fbb7fb1d
t(h)inkerer <thinkerer@mennisch.net>
2022-02-17 05:42:10 -0500

    Cleanup starship

--- modules/starship.nix
+++ modules/starship.nix
@@ -1,7 +1,11 @@
 { config, lib, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ starship ];
   programs = {
-    bash.interactiveShellInit = ''eval "$(starship init bash)"'';
-    zsh.shellInit = ''eval "$(starship init zsh)"'';
+    bash.interactiveShellInit = ''
+      eval "$(starship init bash)"
+    '';
+    zsh.shellInit = ''
+      eval "$(starship init zsh)"
+    '';
   };
 }
