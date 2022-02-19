80add149459c584e4a06b512dcc4763afad5312b
t(h)inkerer <thinkerer@mennisch.net>
2022-02-19 08:28:27 -0500

    Move as much config as possible into config.json
    Restic will back it up, and there's no point in having an environment file when I can just
    use config.json.

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -23,17 +23,10 @@
     vaultwarden = {
       config = {
         DATABASE_URL = "postgresql:///vaultwarden";
-        REQUIRE_DEVICE_EMAIL = false;
         ROCKET_PORT = 8000;
-        SIGNUPS_ALLOWED = false;
-        SIGNUPS_VERIFY = true;
-        SMTP_HOST = "email-smtp.us-east-1.amazonaws.com";
-        SMTP_PORT = 587;
-        SMTP_SSL = true;
       };
       dbBackend = "postgresql";
       enable = true;
-      environmentFile = "/var/lib/bitwarden_rs/config";
     };
   };
 }
